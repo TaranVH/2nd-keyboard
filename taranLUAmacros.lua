4a7983e4e5e77958c7550b3209b809028e1ff2d0
zurbo <zurbo00@gmail.com>
2016-04-09 00:06:32 +0200

    a 'litle' update (lol)

--- taranLUAmacros.lua
+++ taranLUAmacros.lua
@@ -1,135 +1,85 @@
--- get luamacros here: http://www.hidmacros.eu/
--- plug in your 2nd keyboard, load this script into LUAmacros, and press the triangle PLAY button.
--- Then, press any key on that keyboard to assign logical name ('MACROS') to macro keyboard
--- When done this way, you have to reassign the name to your 2nd keyboard every time you open LUAmacros, using the play button located above.
--- There may be a better, more permanent solution, but I don't know it.
-lmc_assign_keyboard('MACROS');
+function file_exist(name)
+   local file=io.open(name,"r");
+   if (file ~= nil) then
+      io.close(file);
+      return true;
+   else
+       return false;
+   end;
+end;
 
-sendToAHK = function (key)
-      print('It was assigned string:    ' .. key)
-      local file = io.open("C:\\Users\\TaranVanHemert\\Documents\\keypressed.txt", "w") -- writing this string to a text file on disk is probably NOT the best method. Feel free to program something better!
-      --print("we are inside the text file")
-      file:write(key)
-      file:flush()
-      file:close()
-      lmc_send_keys('{F24}')  -- using the F24 key to trigger AutoHotKey is probably NOT the best method. Feel free to program something better!
-end
+function sendToAHK(key)
+      print('It was assigned string: ' .. key);
+      local file = io.open(file_path, "w");
+      file:write (key);
+      file:flush ();
+      file:close ();
+      lmc_send_keys('{F24}');
+end;
 
-local config = {
-	[45]  = "insert",
-	[36]  = "home",
-	[33]  = "pageup",
-	[46]  = "delete",
-	[35]  = "end",
-	[34]  = "pagedown",
-	[27]  = "escape",
-	[112] = "F1",
-	[113] = "F2",
-	[114] = "F3",
-	[115] = "F4",
-	[116] = "F5",
-	[117] = "F6",
-	[118] = "F7",
-	[119] = "F8",
-	[120] = "F9",
-	[121] = "F10",
-	[122] = "F11",
-	[123] = "F12",
-	[8]   = "backspace",
-	[220] = "backslash",
-	[13]  = "enter",
-	[16]  = "rShift",
-	[17]  = "rCtrl",
-	[38]  = "up",
-	[37]  = "left",
-	[40]  = "down",
-	[39]  = "right",
-	[32]  = "space",
-	[186] = "semicolon",
-	[222] = "singlequote",
-	[190] = "period",
-	[191] = "slash",
-	[188] = "comma",
-	[219] = "leftbracket",
-	[221] = "rightbracket",
-	[189] = "minus",
-	[187] = "equals",
-	[96]  = "num0",
-	[97]  = "num1",
-	[98]  = "num2",
-	[99]  = "num3",
-	[100] = "num4",
-	[101] = "num5",
-	[102] = "num6",
-	[103] = "num7",
-	[104] = "num8",
-	[105] = "num9",
+function get_script_path()
+         local path = lmc_get_window_title():gsub("LuaMacros ", ""):gsub("- ", "");
+         path = string.match(path, "(.-)([^\\/]-%.?([^%.\\/]*))$");
+         return (path);
+end;
 
-	[106] = "numMult",
-        [107] = "numDelete",
-        -- 108 is unknown...?
-	[109] = "numMult",
-        [110] = "numDelete",
-	[111] = "numDiv",
-        [144] = "numLock", --probably it is best to avoid this key. I keep numlock ON, or it has unexpected effects
+modifiers = {
+        [16]  = "16",
+	[17]  = "17",
+        [18]  = "18"
+};
+difference = {
+	[19]  = "19",
+	[44]  = "44"
+};
 
-        [192] = "`",  --this is the tilde key just before the number row
-        [9]   = "tab",
-        [20]  = "capslock",
-        [18]  = "alt",
-
-
-	[string.byte('Q')] = "q",
-	[string.byte('W')] = "w",
-	[string.byte('E')] = "e",
-	[string.byte('R')] = "r",
-	[string.byte('T')] = "t",
-	[string.byte('Y')] = "y",
-	[string.byte('U')] = "u",
-	[string.byte('I')] = "i",
-	[string.byte('O')] = "o",
-	[string.byte('P')] = "p",
-	[string.byte('A')] = "a",
-	[string.byte('S')] = "s",
-	[string.byte('D')] = "d",
-	[string.byte('F')] = "f",
-	[string.byte('G')] = "g",
-	[string.byte('H')] = "h",
-	[string.byte('J')] = "j",
-	[string.byte('K')] = "k",
-	[string.byte('L')] = "l",
-	[string.byte('Z')] = "z",
-	[string.byte('X')] = "x",
-	[string.byte('C')] = "c",
-	[string.byte('V')] = "v",
-	[string.byte('B')] = "b",
-	[string.byte('N')] = "n",
-	[string.byte('M')] = "m",
-
-        [string.byte('0')] = "0",
-	[string.byte('1')] = "1",
-	[string.byte('2')] = "2",
-	[string.byte('3')] = "3",
-	[string.byte('4')] = "4",
-	[string.byte('5')] = "5",
-        [string.byte('6')] = "6",
-        [string.byte('7')] = "7",
-        [string.byte('8')] = "8",
-        [string.byte('9')] = "9",
-
-	--[255] = "printscreen" --these keys do not work
-}
-
--- define callback for whole device
-lmc_set_handler('MACROS', function(button, direction)
-	if (direction == 1) then return end  -- ignore down -- I believe this also has the effect of neutralizing the modifier keys, unfortunately. Not optimal.
-
-	if type(config[button]) == "string" then
-                print(' ')
-                print('Your key ID number is:   ' .. button)
-		sendToAHK(config[button])
-	else
-                print(' ')
-                print('Not yet assigned: ' .. button)
-	end
-end)
+local path = get_script_path();
+if (not file_exist(path .. "options.opt")) then
+    local file = io.open(path .. "options.opt", "a");
+    file:write ("file_path = \"" .. path:gsub("\\", "\\\\") .. "keypressed.txt\";\n");
+    file:write ("layout_file = \"TARAN.lyt\";\n");
+    file:write ("toggle_modifiers = false;\n");
+    file:write ("differ_pause_prtscr = false;\n");
+    file:write ("toggle_specials_keys_repeat = false;\n");
+    file:write ("special = {\n    [20]  = function () lmc_send_keys(\"{CAPSLOCK}\") end,\n    [144] = function () lmc_send_keys(\"{NUMLOCK}\") end,\n    [145] = function () lmc_send_keys(\"{SCROLLLOCK}\") end\n};\n")
+    file:write ("nb_macros_kb = 1;\n");
+    file:flush ();
+    file:close ();
+end;
+dofile (path .. "options.opt");
+if (layout_file and file_exist(path .. "layouts\\" .. layout_file)) then
+   dofile (path .. "layouts\\" .. layout_file);
+else
+   layout_file = false;
+end;
+lmc_assign_keyboard("MACROS");
+mod_key = "";
+diff_key = "";
+lmc_set_handler("MACROS", function(button, direction)
+        print(button .. ' + '..  direction);
+        if (direction == 1 and toggle_modifiers and modifiers[button]) then
+            mod_key = modifiers[button];
+        elseif (direction == 0 and toggle_modifiers and modifiers[button]) then
+             mod_key = "";
+        elseif (direction == 0 and differ_pause_prtscr and difference[button]) then
+             button = difference[button];
+             if (mod_key ~= "") then
+                sendToAHK(mod_key .. "+" .. button);
+             else
+                 sendToAHK(button);
+             end;
+        elseif (direction == 0 and ((not layout_file) or layout[button]) and ((not toggle_modifiers) or (not modifiers[button])) and ((not differ_pause_prtscr) or button ~= 255)) then
+             local key = button;
+             if (layout_file) then
+                key = layout[button];
+             end;
+             if (mod_key ~= "") then
+                sendToAHK(mod_key .. "+" .. key);
+             else
+                 sendToAHK(key);
+             end;
+             if (toggle_specials_keys_repeat and special[button]) then
+                 special[button]();
+             end;
+        end;
+end);
