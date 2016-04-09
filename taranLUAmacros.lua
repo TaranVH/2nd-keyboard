7977284cbf6203f711ef00cb71925a863549b6f0
zurbo <zurbo00@gmail.com>
2016-04-09 11:05:38 +0200

    added a space in the command execution

--- taranLUAmacros.lua
+++ taranLUAmacros.lua
@@ -12,7 +12,7 @@ function sendToAHK(key, button)
 	print("you pressed the key ID: " .. button)
 	print("It was assigned string: " .. key);
 	if (use_command == true) then --if you prefer to passes the keypress though command line arguments the send_keypress in the options file should be the command you want to execute
-		os.execute(send_keypress .. key);
+		os.execute(send_keypress .. " " .. key);
 	else --else send_keypress should be an ABSOLUTE path to the file containing the keypress
 		local file = io.open(send_keypress, "w");
 		file:write(key);
